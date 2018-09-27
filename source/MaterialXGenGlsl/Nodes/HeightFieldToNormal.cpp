842fa2d68aac6764be7cca756bb15067c5f6afbe
Bernard Kwok <Bernard.Kwok@autodesk.com>
2018-09-27 17:11:41 -0400

    Shader code cleanup.

--- source/MaterialXGenGlsl/Nodes/HeightFieldToNormal.cpp
+++ source/MaterialXGenGlsl/Nodes/HeightFieldToNormal.cpp
@@ -26,41 +26,15 @@ namespace MaterialX
 
         BEGIN_SHADER_STAGE(shader, HwShader::PIXEL_STAGE)
 
-            // Emit function signature. Need to add filter width and filter height
-            shader.addLine("vec3 IM_heighttonormal_vector3_sx_glsl(float _in[9], float _scale, float _width, float _height)"
-                , false);
+        // Emit function signature. Need to add filter width and filter height
+        shader.addLine("vec3 IM_heighttonormal_vector3_sx_glsl(float K[9], float _scale, float _width, float _height)", false);
         shader.beginScope();
-
-        vector<string> kernalStrings;
-        kernalStrings.push_back("K[0] = ");
-        kernalStrings.push_back("K[1] = ");
-        kernalStrings.push_back("K[2] = ");
-
-        kernalStrings.push_back("K[3] = ");
-        kernalStrings.push_back("K[4] = ");
-        kernalStrings.push_back("K[5] = ");
-
-        kernalStrings.push_back("K[6] = ");
-        kernalStrings.push_back("K[7] = ");
-        kernalStrings.push_back("K[8] = ");
-
-        string inValueString = "_in";
-        for (unsigned int i = 0; i < kernalStrings.size(); i++)
-        {
-            kernalStrings[i].append(inValueString + "[" + std::to_string(i) + "];");
-        }
-
-        shader.addLine("float K[9];");
-        for (auto kernalString : kernalStrings)
-        {
-            shader.addLine(kernalString);
-        }
 
         // Sobel filter computation. Can change this
         std::vector<std::string> computeFilter;
         computeFilter.push_back("float nx = K[2] + (2.0*K[5]) + K[8] - (K[0] + (2.0*K[3]) + K[6]);");
         computeFilter.push_back("float ny = K[0] + (2.0*K[1]) + K[2] - (K[6] + (2.0*K[7]) + K[8]);");
-        computeFilter.push_back("float nz = _scale * sqrt(1.0 - dx*dx - dy*dy);");
+        computeFilter.push_back("float nz = _scale * sqrt(1.0 - nx*nx - ny*ny);");
         computeFilter.push_back("return vec3(nx, ny, nz);");
         for (auto f : computeFilter)
         {
@@ -73,7 +47,7 @@ namespace MaterialX
         END_SHADER_STAGE(shader, HwShader::PIXEL_STAGE)
     }
 
-    void HeightFieldToNormalGlsl::emitFunctionCall(const SgNode& node, const SgNodeContext& context, ShaderGenerator& shadergen, Shader& shader_)
+    void HeightFieldToNormalGlsl::emitFunctionCall(const SgNode& node, const SgNodeContext& /*context*/, ShaderGenerator& shadergen, Shader& shader_)
     {
         HwShader& shader = static_cast<HwShader&>(shader_);
 
@@ -118,18 +92,18 @@ namespace MaterialX
         }
 
         // Dump out kernal setting code
-        shader.beginLine();
 
         // TODO: Determine how to specify these values
         string filterWidth("0.1");
         string filterHeight("0.1");
 
-        string kernalName("_kernal" + context.getFunctionSuffix()); // Does this make it unique?
+        string kernalName(node.getOutput()->name + "_kernal"); 
         shader.addLine("float " + kernalName + "[9]"); 
         for (unsigned int i=0; i<9; i++)
         {
-            shader.addLine(kernalName + "[" + kernalStrings[i] + "]");
+            shader.addLine(kernalName + "[" + std::to_string(i) + "] = " + kernalStrings[i]);
         }
+        shader.beginLine();
         shadergen.emitOutput(node.getOutput(), true, shader);
         shader.addStr(" = IM_heighttonormal_vector3_sx_glsl");
         shader.addStr("(" + kernalName + ", " + scaleValueString + ", " + filterWidth + ", " + filterHeight + ")");
