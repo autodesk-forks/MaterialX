9704a03c01835e34330105c279c68254f98fa5e5
Bernard Kwok <Bernard.Kwok@autodesk.com>
2018-10-12 15:40:01 -0400

    Initialize re-work for variable size kernels .

--- documents/Libraries/stdlib/sx-glsl/lib/sx_sampling.glsl
+++ documents/Libraries/stdlib/sx-glsl/lib/sx_sampling.glsl
@@ -1,3 +1,6 @@
+// Restrict to 7x7 kernel size for performance reasons
+#define SX_MAX_SAMPLE_COUNT = 49
+
 //
 // Function to compute the sample size relative to a texture coordinate
 //
@@ -29,70 +32,119 @@ vec3 sx_normal_from_samples_sobel(float S[9], float _scale)
    return (norm + 1.0) * 0.5;
 };
 
-//
-// Blur using box filter for float samples
-//
-float sx_blur_box_float(float S[9])
+// Kernal weights for box filter
+void sx_get_box_weights(inout float W[SX_MAX_SAMPLE_COUNT], int filterSize)
 {
-    return (S[0] + S[1] + S[2] + S[3] + S[4] + S[5] + S[6] + S[7] + S[8]) / 9.0;
+    int filterSize2 = filterSize*filterSize;
+    for (int i=0; i<filterSize2; i++)
+    {
+        W[i] = 1.0 / float(filterSize2);
+    }
+    for (int j=filterSize2; j<SX_MAX_SAMPLE_COUNT]; j++)
+    {
+        W[j] = 0.0;
+    }
+}
+
+// Kernel weights for Gaussian filter. Sigma is assumed to be 1.
+void sx_get_gaussian_weights(inout float W[SX_MAX_SAMPLE_COUNT], int filterSize)
+{
+    if (filterSize == 1)
+    {
+        W[0] = 1.0;
+    }
+    else if (filterSize == 3)
+    {
+        W = float[](
+            0.0625, 0.125, 0.0625,
+            0.125,  0.25,  0.125,
+            0.0625, 0.125, 0.0625);
+    }
+    else if (filterSize == 5)
+    {
+        W = float[](
+            0.003765,	0.015019,	0.023792,	0.015019,	0.003765,
+            0.015019,	0.059912,	0.094907,	0.059912,	0.015019,
+            0.023792,	0.094907,	0.150342,	0.094907,	0.023792,
+            0.015019,	0.059912,	0.094907,	0.059912,	0.015019,
+            0.003765,	0.015019,	0.023792,	0.015019,	0.003765
+            );
+    }
+    else if (filterSize == 7)
+    {
+        W = float[](
+            0.000036,	0.000363,	0.001446,	0.002291,	0.001446,	0.000363,	0.000036,
+            0.000363,	0.003676,	0.014662,	0.023226,	0.014662,	0.003676,	0.000363,
+            0.001446,	0.014662,	0.058488,	0.092651,	0.058488,	0.014662,	0.001446,
+            0.002291,	0.023226,	0.092651,	0.146768,	0.092651,	0.023226,	0.002291,
+            0.001446,	0.014662,	0.058488,	0.092651,	0.058488,	0.014662,	0.001446,
+            0.000363,	0.003676,	0.014662,	0.023226,	0.014662,	0.003676,	0.000363,
+            0.000036,	0.000363,	0.001446,	0.002291,	0.001446,	0.000363,	0.000036
+        );
+    }
+    else
+    {
+        filterSize = 0;
+    }
+
+    for (int j=filterSize; j<SX_MAX_SAMPLE_COUNT]; j++)
+    {
+        W[j] = 0.0;
+    }
 }
 
 //
-// Blur using box filter for vec2 samples
+// Apply filter for float samples S, using weights W.
+// sampleCount should be a square of a odd number in the range { 1, 3, 5, 7 }
 //
-vec2 sx_blur_box_vec2(vec2 S[9])
+float sx_convolution_float(float S[SX_MAX_SAMPLE_COUNT], float W[SX_MAX_SAMPLE_COUNT], int sampleCount)
 {
-    return (S[0] + S[1] + S[2] + S[3] + S[4] + S[5] + S[6] + S[7] + S[8]) / 9.0;
+    float result = 0.0;
+    for (int = 0;  i < sampleCount; ++i)
+    {
+        result += S[i]*W[i];
+    }
+    return result;
 }
 
 //
-// Blur using box filter for vec3 samples
+// Apply filter for vec2 samples S, using weights W.
+// sampleCount should be a square of a odd number in the range { 1, 3, 5, 7 }
 //
-vec3 sx_blur_box_vec3(vec3 S[9])
+vec2 sx_convolution_vec2(vec2 S[SX_MAX_SAMPLE_COUNT], float W[SX_MAX_SAMPLE_COUNT], float sampleCount)
 {
-    return (S[0] + S[1] + S[2] + S[3] + S[4] + S[5] + S[6] + S[7] + S[8]) / 9.0;
+    vec2 result = vec2(0.0);
+    for (int = 0;  i < sampleCount; ++i)
+    {
+        result += S[i]*W[i];
+    }
+    return result;
 }
 
 //
-// Blur using box filter for vec4 samples
+// Apply filter for vec3 samples S, using weights W.
+// sampleCount should be a square of a odd number in the range { 1, 3, 5, 7 }
 //
-vec4 sx_blur_box_vec4(vec4 S[9])
+vec3 sx_convolution_vec3(vec3 S[SX_MAX_SAMPLE_COUNT], float W[SX_MAX_SAMPLE_COUNT], float sampleCount)
 {
-    return (S[0] + S[1] + S[2] + S[3] + S[4] + S[5] + S[6] + S[7] + S[8]) / 9.0;
+    vec3 result = vec2(0.0);
+    for (int = 0;  i < sampleCount; ++i)
+    {
+        result += S[i]*W[i];
+    }
+    return result;
 }
 
 //
-// Blur using box filter for float samples
+// Apply filter for vec4 samples S, using weights W.
+// sampleCount should be a square of a odd number { 1, 3, 5, 7 }
 //
-float sx_blur_gaussian_float(float S[9])
+vec4 sx_convolution_vec4(vec4 S[SX_MAX_SAMPLE_COUNT], float W[SX_MAX_SAMPLE_COUNT], float sampleCount)
 {
-    float W[9] = { 0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625 };
-    return (S[0]*W[0] + S[1]*W[1] + S[2]*W[2] + S[3]*W[3] + S[4]*W[4] + S[5]*W[5] + S[6]*W[6] + S[7]*W[7] + S[8]*W[8]);
-}
-
-//
-// Blur using gaussian filter for vec2 samples
-//
-vec2 sx_blur_gaussian_vec2(vec2 S[9])
-{
-    float W[9] = { 0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625 };
-    return (S[0]*W[0] + S[1]*W[1] + S[2]*W[2] + S[3]*W[3] + S[4]*W[4] + S[5]*W[5] + S[6]*W[6] + S[7]*W[7] + S[8]*W[8]);
-}
-
-//
-// Blur using gaussian filter for vec3 samples
-//
-vec3 sx_blur_gaussian_vec3(vec3 S[9])
-{
-    float W[9] = { 0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625 };
-    return (S[0]*W[0] + S[1]*W[1] + S[2]*W[2] + S[3]*W[3] + S[4]*W[4] + S[5]*W[5] + S[6]*W[6] + S[7]*W[7] + S[8]*W[8]);
-}
-
-//
-// Blur using gaussian filter for vec4 samples
-//
-vec4 sx_blur_gaussian_vec4(vec4 S[9])
-{
-    float W[9] = { 0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625 };
-    return (S[0]*W[0] + S[1]*W[1] + S[2]*W[2] + S[3]*W[3] + S[4]*W[4] + S[5]*W[5] + S[6]*W[6] + S[7]*W[7] + S[8]*W[8]);
+    vec4 result = vec2(0.0);
+    for (int = 0;  i < sampleCount; ++i)
+    {
+        result += S[i]*W[i];
+    }
+    return result;
 }
--- source/MaterialXGenGlsl/Nodes/BlurGlsl.cpp
+++ source/MaterialXGenGlsl/Nodes/BlurGlsl.cpp
@@ -22,13 +22,16 @@ SgImplementationPtr BlurGlsl::create()
 
 void BlurGlsl::computeSampleOffsetStrings(const string& sampleSizeName, StringVec& offsetStrings)
 {
-    // Build a 3x3 grid of samples that are offset by the provided sample size
     offsetStrings.clear();
+ 
+    // Build a NxN grid of samples that are offset by the provided sample size
+    int offset = ((int)(sqrt(_sampleCount)) - 1) / 2;
+
     if (_filterType == "box")
     {
-        for (int row = -1; row <= 1; row++)
+        for (int row = -offset; row <= offset; row++)
         {
-            for (int col = -1; col <= 1; col++)
+            for (int col = -offset; col <= offset; col++)
             {
                 offsetStrings.push_back(" + " + sampleSizeName + " * vec2(" + std::to_string(float(col)) + "," + std::to_string(float(row)) + ")");
             }
@@ -71,48 +74,77 @@ void BlurGlsl::emitFunctionCall(const SgNode& node, SgNodeContext& context, Shad
         throw ExceptionShaderGenError("Node '" + node.getName() + "' is not a valid Blur node");
     }
 
-    // Check for type of filter to apply
-    //
-    const string BOX_FILTER("box");
-    const string GAUSSIAN_FILTER("gaussian");
-    if (filterTypeInput->value)
+    // Check size of filter.
+    const SgInput* sizeInput = node.getInput("size");     
+    unsigned int filterSize = 3;
+    if (sizeInput)
     {
-        if (filterTypeInput->value->getValueString() == GAUSSIAN_FILTER)
+        float sizeInputValue = sizeInput->value->asA<float>();
+        if (sizeInputValue <= 0.25f)
         {
-            // TODO: Add in Gaussian support
-            _filterType = GAUSSIAN_FILTER;
-            _sampleCount = 9;
+            filterSize = 3;
         }
-        else 
+        if (sizeInputValue <= 0.55f)
         {
-            _filterType = BOX_FILTER;
-            _sampleCount = 9;
+            filterSize = 5;
+        }
+        else
+        {
+            filterSize = 7;
         }
     }
 
-    // Check size of filter. TO-DO.
-    //const SgInput* sizeInput = node.getInput("size");     
+    // Check for type of filter to apply
+    // Default to box filter
+    //
+    const string BOX_FILTER("box");
+    const string GAUSSIAN_FILTER("gaussian");
+    _filterType = BOX_FILTER;
+    string weightFunction = "sx_get_box_weights";
+    if (filterTypeInput->value)
+    {
+        // Use Gaussian filter.
+        if (filterTypeInput->value->getValueString() == GAUSSIAN_FILTER)
+        {
+            _filterType = GAUSSIAN_FILTER;
+            weightFunction = "sx_get_gaussian_weights";
+        }
+    }
+
+    // Sample count is quare of filter size
+    _sampleCount = filterSize*filterSize;
 
     HwShader& shader = static_cast<HwShader&>(shader_);
 
     BEGIN_SHADER_STAGE(shader, HwShader::PIXEL_STAGE)
     {
         // Emit samples
+        // Note: The maximum sample count SX_MAX_SAMPLE_COUNT is defined in the shader code and 
+        // is assumed to be 49 (7x7 kernel). If this changes that the filter size logic here 
+        // needs to be adjusted.
+        //
         StringVec sampleStrings;
         emitInputSamplesUV(node, context, shadergen, shader, sampleStrings);
 
+        // Set up sample array
         string sampleName(node.getOutput()->name + "_samples");
-        shader.addLine(_inputTypeString + " " + sampleName + "[" + std::to_string(_sampleCount) + "]");
+        shader.addLine(_inputTypeString + " " + sampleName + "[SX_MAX_SAMPLE_COUNT]");
         for (unsigned int i = 0; i < _sampleCount; i++)
         {
             shader.addLine(sampleName + "[" + std::to_string(i) + "] = " + sampleStrings[i]);
         }
 
-        // Emit code to evaluate samples. Function to call depends on input type.
+        // Set up weight array
+        string weightName(node.getOutput()->name + "_weights");
+        shader.addLine(_inputTypeString + " " + weightName + "[SX_MAX_SAMPLE_COUNT]");
+        shader.addLine(weightFunction + "(" + weightName + ", " + std::to_string(_sampleCount) + ")");
+
+        // Emit code to evaluate using input sample and weight arrays. 
+        // The function to call depends on input type.
         //
         shader.beginLine();
         shadergen.emitOutput(context, node.getOutput(), true, false, shader);
-        _filterFunctionName = "sx_blur_" + _filterType  + "_" + _inputTypeString;
+        _filterFunctionName = "sx_convolution_" + _filterType  + "_" + _inputTypeString;
         shader.addStr(" = " + _filterFunctionName);
         shader.addStr("(" + sampleName + ")");
         shader.endLine();
