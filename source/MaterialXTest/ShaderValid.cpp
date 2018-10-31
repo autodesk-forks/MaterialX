e01bdaa1ad8308beda484e704469b2f965cc3e92
Bernard Kwok <Bernard.Kwok@autodesk.com>
2018-10-31 14:47:53 -0400

    Cleanup.

--- source/MaterialXTest/ShaderValid.cpp
+++ source/MaterialXTest/ShaderValid.cpp
@@ -454,14 +454,6 @@ static void runGLSLValidation(const std::string& shaderName, mx::TypedElementPtr
             std::string fileName = shaderPath + ".exr";
             validator.save(fileName);
 
-            std::ofstream file;
-            file.open(shaderPath + ".vert");
-            file << shader->getSourceCode(mx::HwShader::VERTEX_STAGE);
-            file.close();
-            file.open(shaderPath + ".frag");
-            file << shader->getSourceCode(mx::HwShader::PIXEL_STAGE);
-            file.close();
-
             validated = true;
         }
         catch (mx::ExceptionShaderValidationError e)
@@ -644,15 +636,6 @@ TEST_CASE("MaterialX documents", "[shadervalid]")
     // which files in the test suite are being tested.
     // Add only the test suite filename not the full path.
     std::set<std::string> testfileOverride;
-    //testfileOverride.insert("triplanarprojection.mtlx");
-    //testfileOverride.insert("blur.mtlx");
-    testfileOverride.insert("image.mtlx");
-    //testfileOverride.insert("tiledimage.mtlx");
-    testfileOverride.insert("surfaceshader.mtlx");
-    //testfileOverride.insert("streams.mtlx");
-    testfileOverride.insert("channel.mtlx");
-    //testfileOverride.insert("adjustment.mtlx");
-    testfileOverride.insert("nodegraph_standardsurface.mtlx");    
 
     // Library search path
     mx::FilePath searchPath = mx::FilePath::getCurrentPath() / mx::FilePath("documents/Libraries");
