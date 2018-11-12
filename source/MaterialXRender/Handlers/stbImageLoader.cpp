364c8522eefe3cdd36c529739b2642283a498cdc
Bernard Kwok <Bernard.Kwok@autodesk.com>
2018-11-12 16:56:04 -0500

    Build fixes.

--- source/MaterialXRender/Handlers/stbImageLoader.cpp
+++ source/MaterialXRender/Handlers/stbImageLoader.cpp
@@ -1,6 +1,6 @@
 #include <MaterialXRender/Window/HardwarePlatform.h>
 
-#if defined(OSWin_) |
+#if defined(OSWin_)
 #pragma warning( push )
 #pragma warning( disable: 4100)
 #endif
@@ -10,14 +10,11 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
-#endif
 
 #include <MaterialXRender/Handlers/stbImageLoader.h>
 
 namespace MaterialX
 {
-#if defined(STB_DEFINE) 
-
 bool stbImageLoader::saveImage(const std::string& fileName,
                                     const ImageDesc& imageDesc)
 {
@@ -26,28 +23,28 @@ bool stbImageLoader::saveImage(const std::string& fileName,
     int w = static_cast<int>(imageDesc.width);
     int h = static_cast<int>(imageDesc.height);
     int channels = static_cast<int>(imageDesc.channelCount);
-    //returnValue = SaveEXR(imageDesc.resourceBuffer, , , , 1 /* save as 16 bit float format */, fileName.c_str());
-    // Fail with any type other than exr.
+    void* data = imageDesc.resourceBuffer;
+
     std::string extension = (fileName.substr(fileName.find_last_of(".") + 1));
     if (extension == "png")
     {
-        returnValue = stbi_write_png(fileName.c_str(), fname), w, h, channels, imageDesc.res, w * 4);
+        returnValue = stbi_write_png(fileName.c_str(), w, h, channels, data, w * 4);
     }
     else if (extension == "bmp")
     {
         returnValue = stbi_write_bmp(fileName.c_str(), w, h, channels, data);
     }
-    eise if (extension == "tga")
+    else if (extension == "tga")
     { 
         returnValue = stbi_write_tga(fileName.c_str(), w, h, channels, data);
     }
-    eise if (extension == "jpg" || (extension == "jpeg")
+    else if (extension == "jpg" || extension == "jpeg")
     {
-        returnValue = stbi_write_jpg(fileName.c_str(), w, h, channels, imageDesc.resourceBuffer, 100);
+        returnValue = stbi_write_jpg(fileName.c_str(), w, h, channels, data, 100);
     }
-    eise if (extension == "hdr")
+    else if (extension == "hdr")
     {
-        returnValue = stbi_write_hdr(fileName.c_str(), w, h, channels, static_cast<float>(imageDesc.resourceBuffer));
+        returnValue = stbi_write_hdr(fileName.c_str(), w, h, channels, static_cast<float*>(data));
     }
     return (returnValue == 1);
 }
@@ -91,21 +88,6 @@ bool stbImageLoader::acquireImage(const std::string& fileName,
 #if defined(OSWin_)
 #pragma warning( pop ) 
 #endif
-
-#else
-bool stbImageLoader::saveImage(const std::string& /*fileName*/,
-    const ImageDesc& /*imageDesc*/) 
-{
-    return false;
-}
-bool stbImageLoader::acquireImage(const std::string& /*fileName*/,
-    ImageDesc& /*imageDesc*/,
-    bool /*generateMipMaps*/) 
-{
-    return false;
-}
-#endif
-
 
 }
 
