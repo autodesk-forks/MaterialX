fb9626bde5a31007f804c0cc4e455a36ddc23e28
Bernard Kwok <Bernard.Kwok@autodesk.com>
2018-11-14 09:34:39 -0500

    Add stb include files directly instead of using a submodule dependency.

--- source/MaterialXRender/Handlers/stbImageLoader.cpp
+++ source/MaterialXRender/Handlers/stbImageLoader.cpp
@@ -6,10 +6,10 @@
 #endif
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb/stb_image_write.h>
+#include <MaterialXRender/External/stb/stb_image_write.h>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
+#include <MaterialXRender/External/stb/stb_image.h>
 
 #include <MaterialXRender/Handlers/stbImageLoader.h>
 
