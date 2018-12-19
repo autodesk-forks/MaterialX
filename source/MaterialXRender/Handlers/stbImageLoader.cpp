b5a42a1e337ea8878581a5542bfa50518aaa56a4
Bernard Kwok <Bernard.Kwok@autodesk.com>
2018-12-18 19:15:35 -0500

    Fix 32-bit build.

--- source/MaterialXRender/Handlers/stbImageLoader.cpp
+++ source/MaterialXRender/Handlers/stbImageLoader.cpp
@@ -22,7 +22,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <MaterialXRender/External/stb/stb_image.h>
 
-#if defined(OSWin_)
+#if defined(OSWin_) || defined(_WIN32)
     #pragma warning( pop ) 
 #elif defined(__clang__)
     #pragma clang diagnostic pop
