32d58492af23d76e294f6cf8ac84973b43303ddf
Bernard Kwok <Bernard.Kwok@autodesk.com>
2019-01-10 11:31:45 -0500

    Rename testsuite folder.

--- documents/Libraries/stdlib/gen-glsl/mx_cellnoise2d_float.glsl
+++ documents/Libraries/stdlib/gen-glsl/mx_cellnoise2d_float.glsl
@@ -1,4 +1,4 @@
-#include "stdlib/gsn-glsl/lib/mx_noise.glsl"
+#include "stdlib/gen-glsl/lib/mx_noise.glsl"
 
 void mx_cellnoise2d_float(vec2 texcoord, out float result)
 {
--- documents/Libraries/stdlib/gen-glsl/mx_cellnoise3d_float.glsl
+++ documents/Libraries/stdlib/gen-glsl/mx_cellnoise3d_float.glsl
@@ -1,4 +1,4 @@
-#include "stdlib/gsn-glsl/lib/mx_noise.glsl"
+#include "stdlib/gen-glsl/lib/mx_noise.glsl"
 
 void mx_cellnoise3d_float(vec3 position, out float result)
 {
