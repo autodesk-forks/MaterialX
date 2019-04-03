56efc3b86170743370b74fc0239a25e005df57fe
Bernard Kwok <Bernard.Kwok@autodesk.com>
2019-04-03 14:29:55 -0400

    Debug cleanup.

--- source/MaterialXRender/Handlers/ImageHandler.cpp
+++ source/MaterialXRender/Handlers/ImageHandler.cpp
@@ -81,7 +81,6 @@ bool ImageHandler::saveImage(const FilePath& filePath,
             bool saved = iter->second->saveImage(foundFilePath, imageDesc, verticalFlip);
             if (saved)
             {
-                std::cout << "Saved image: " << foundFilePath.asString() << std::endl;
                 return true;
             }
         }
@@ -91,9 +90,7 @@ bool ImageHandler::saveImage(const FilePath& filePath,
 
 bool ImageHandler::acquireImage(const FilePath& filePath, ImageDesc &imageDesc, bool /*generateMipMaps*/, const Color4* /*fallbackColor*/)
 {
-    std::cout << "ImageHandler::acquireImage: START\n";
     FilePath foundFilePath = findFile(filePath);
-    std::cout << "ImageHandler::acquireImage: foundFilePath = " << foundFilePath.asString() << std::endl;
     if (foundFilePath.isEmpty())
     {
         return false;
@@ -106,31 +103,13 @@ bool ImageHandler::acquireImage(const FilePath& filePath, ImageDesc &imageDesc, 
         ImageLoaderPtr loader = iter->second;
         if (loader && loader->supportedExtensions().count(extension))
         {
-            std::cout << "-- Use loader to acquire \n";
             bool acquired = loader->acquireImage(foundFilePath, imageDesc, getRestrictions());
             if (acquired)
             {
-                std::cout << "ImageHandler::acquireImage: FOUND END\n";
                 return true;
             }
         }
     }
-#if 0
-    std::pair <::iterator, ImageLoaderMap::iterator> range;
-    range = _imageLoaders.equal_range(extension);
-    ImageLoaderMap::iterator first = --range.second;
-    ImageLoaderMap::iterator last= --range.first;
-    for (auto it = first; it != last; --it)
-    {
-        std::cout << "-- Use loader to acquire \n";
-        if (acquired)
-        {
-            std::cout << "ImageHandler::acquireImage: FOUND END\n";
-            return true;
-        }
-    }
-#endif
-    std::cout << "ImageHandler::acquireImage: NOT FOUND END\n";
     return false;
 }
 
--- source/MaterialXTest/Render.cpp
+++ source/MaterialXTest/Render.cpp
@@ -1211,7 +1211,6 @@ struct ImageHandlerTestOptions
 void testImageHandler(ImageHandlerTestOptions& options)
 {
     mx::FilePath imagePath = mx::FilePath::getCurrentPath() / mx::FilePath("resources/Images/");
-    std::cout << "Read from image path: " << imagePath.asString() << std::endl;
     mx::FilePathVec files;
 
     unsigned int loadFailed = 0;
@@ -1225,13 +1224,11 @@ void testImageHandler(ImageHandlerTestOptions& options)
         for (const mx::FilePath& file : files)
         {
             const mx::FilePath filePath = imagePath / file;
-            std::cout << "Try reading image : " << filePath.asString() << std::endl;
             const std::string& fileName = filePath;
             mx::ImageDesc desc;
             bool loaded = options.imageHandler->acquireImage(filePath, desc, false, nullptr);
             if (options.logFile)
             {
-                std::cout << "Loaded image: " << fileName << ". Loaded: " << loaded << std::endl;
                 *(options.logFile) << "Loaded image: " << fileName << ". Loaded: " << loaded << std::endl;
             }
             if (!loaded)
@@ -1253,20 +1250,15 @@ TEST_CASE("Render: Image Handler Load", "[rendercore]")
         ImageHandlerTestOptions options;
         options.logFile = &imageHandlerLog;
 
-        std::cout << "** Test STB image loader **" << std::endl;
         imageHandlerLog << "** Test STB image loader **" << std::endl;
         mx::StbImageLoaderPtr stbLoader = mx::StbImageLoader::create();
-        std::cout << "-Create handler\n";
         mx::ImageHandlerPtr imageHandler = mx::ImageHandler::create(nullptr);
-        std::cout << "-Add loader \n";
         imageHandler->addLoader(stbLoader);
-        std::cout << "- set options \n";
         options.testExtensions = stbLoader->supportedExtensions();
         options.imageHandler = imageHandler;
         testImageHandler(options);
-        std::cout << "** END Test STB image loader **" << std::endl;
 
-#ifdef MATERIALX_BUILD_CONTRIB_1
+#ifdef MATERIALX_BUILD_CONTRIB
         imageHandlerLog << "** Test TinyEXR image loader **" << std::endl;
         mx::TinyEXRImageLoaderPtr exrLoader = mx::TinyEXRImageLoader::create();
         mx::ImageHandlerPtr imageHandler2 = mx::ImageHandler::create(nullptr);
@@ -1276,7 +1268,7 @@ TEST_CASE("Render: Image Handler Load", "[rendercore]")
         testImageHandler(options);
 #endif
 
-#if defined(MATERIALX_BUILD_OIIO_1) && defined(OPENIMAGEIO_ROOT_DIR)
+#if defined(MATERIALX_BUILD_OIIO) && defined(OPENIMAGEIO_ROOT_DIR)
         imageHandlerLog << "** Test OpenImageIO image loader **" << std::endl;
         mx::OiioImageLoaderPtr oiioLoader = mx::OiioImageLoader::create();
         mx::ImageHandlerPtr imageHandler3 = mx::ImageHandler::create(nullptr);
