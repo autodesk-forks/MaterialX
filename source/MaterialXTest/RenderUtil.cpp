4bc7c73ea6969362e70869614aa46f0f2fe9d885
Bernard Kwok <Bernard.Kwok@autodesk.com>
2019-04-17 14:00:45 -0400

    Generalize Render tests to allow passing in of test root path list + location of options file.

--- source/MaterialXTest/RenderUtil.cpp
+++ source/MaterialXTest/RenderUtil.cpp
@@ -324,14 +324,12 @@ void ShaderRenderTester::printRunLog(const RenderProfileTimes &profileTimes,
     }
 }
 
-bool ShaderRenderTester::validate()
+bool ShaderRenderTester::validate(const mx::FilePathVec& testRootPaths, const mx::FilePath optionsFilePath)
 {
     // Test has been turned off so just do nothing.
     // Check for an option file
-    mx::FilePath testRoot = mx::FilePath::getCurrentPath() / mx::FilePath("resources/Materials/TestSuite");
-    const mx::FilePath optionsPath = testRoot / mx::FilePath("_options.mtlx");
     RenderUtil::RenderTestOptions options;
-    if (!options.readOptions(optionsPath))
+    if (!options.readOptions(optionsFilePath))
     {
         return false;
     }
@@ -373,9 +371,11 @@ bool ShaderRenderTester::validate()
 
     RenderUtil::AdditiveScopedTimer ioTimer(profileTimes.ioTime, "Global I/O time");
     mx::FilePathVec dirs;
-    dirs = testRoot.getSubDirectories();
-    const mx::FilePath standardSurfacePath = mx::FilePath::getCurrentPath() / mx::FilePath("resources/Materials/Examples/StandardSurface");
-    dirs.push_back(standardSurfacePath);
+    for (auto testRoot : testRootPaths)
+    {
+        mx::FilePathVec testRootDirs = testRoot.getSubDirectories();
+        dirs.insert(std::end(dirs), std::begin(testRootDirs), std::end(testRootDirs));
+    }
 
     ioTimer.endTimer();
 
