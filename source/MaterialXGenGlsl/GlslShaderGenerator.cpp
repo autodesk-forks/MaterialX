d8d5206734d92c31e0ab26f77f6cdbb2ff500b43
niklasharrysson <niklas.harrysson@gmail.com>
2019-06-13 22:22:19 +0200

    Renamed setSignature -> setFunctionName, since only the name is set.
    (cherry picked from commit 4a52c3adb477ad31ed32d9ed9e5283d3b5fd9801)

--- source/MaterialXGenGlsl/GlslShaderGenerator.cpp
+++ source/MaterialXGenGlsl/GlslShaderGenerator.cpp
@@ -325,7 +325,8 @@ void GlslShaderGenerator::emitVertexStage(const ShaderGraph& graph, GenContext& 
 
     emitFunctionDefinitions(graph, context, stage);
 
-    // Add main function. Cache the signature for the stage
+    // Add main function
+    setFunctionName("main", stage);
     emitLine("void main()", stage, false);
     emitScopeBegin(stage);
     emitLine("vec4 hPositionWorld = u_worldMatrix * vec4(i_position, 1.0)", stage);
@@ -452,9 +453,9 @@ void GlslShaderGenerator::emitPixelStage(const ShaderGraph& graph, GenContext& c
 
     const ShaderGraphOutputSocket* outputSocket = graph.getOutputSocket();
 
-    // Add main function. Cache the signature for the stage
+    // Add main function
+    setFunctionName("main", stage);
     emitLine("void main()", stage, false);
-    setSignature(stage, "main");
     emitScopeBegin(stage);
 
     if (graph.hasClassification(ShaderNode::Classification::CLOSURE))
