0f433e18fb64c6713ff5469cf8519b01988ca9f2
Bernard Kwok <Bernard.Kwok@autodesk.com>
2019-06-14 08:29:20 -0400

    Fix merge.

--- source/MaterialXView/Viewer.h
+++ source/MaterialXView/Viewer.h
@@ -70,7 +70,6 @@ class Viewer : public ng::Screen
 
     void setupLights(mx::DocumentPtr doc);
     void loadDocument(const mx::FilePath& filename, mx::DocumentPtr libraries);
-    void reloadShaders();
     void saveShaderSource();
     void loadShaderSource();
     void saveDotFiles();
