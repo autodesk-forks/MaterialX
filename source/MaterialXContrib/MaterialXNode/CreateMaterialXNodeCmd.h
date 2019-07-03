cbeaa108b9c356f5b08a60e40b529a5963a7d6f5
Pavlo Penenko <pavlo.penenko@autodesk.com>
2019-07-03 11:10:53 -0400

    Removed attribute creation and setting methods previously used for dynamically generated input attributes.

--- source/MaterialXContrib/MaterialXNode/CreateMaterialXNodeCmd.h
+++ source/MaterialXContrib/MaterialXNode/CreateMaterialXNodeCmd.h
@@ -15,24 +15,19 @@
 class CreateMaterialXNodeCmd : MPxCommand
 {
   public:
-	CreateMaterialXNodeCmd();
-	~CreateMaterialXNodeCmd() override;
+    CreateMaterialXNodeCmd();
+    ~CreateMaterialXNodeCmd() override;
 
-	MStatus doIt( const MArgList& ) override;
-	bool isUndoable() { return false; }
+    MStatus doIt(const MArgList&) override;
+    bool isUndoable() { return false; }
 
-	static MSyntax newSyntax();
-	static void* creator();
+    static MSyntax newSyntax();
+    static void* creator();
 
     static MString NAME;
 
   private:
-	void setAttributeValue(MObject &materialXObject, MObject &attr, const float* values, unsigned int size);
-	void setAttributeValue(MObject &materialXObject, MObject &attr, const std::string& stringValue);
-	void createAttribute(MObject &materialXObject, const std::string& name, const MaterialX::UIPropertyItem& propertyItem);
-	void createAttributes(MObject &materialXObject, const MaterialX::UIPropertyGroup& groups, const MaterialX::UIPropertyGroup& unnamedGroups);
-
-	MDGModifier _dgModifier;
+    MDGModifier _dgModifier;
 };
 
 #endif /* CREATE_MATERIALX_NODE_CMD_H */
