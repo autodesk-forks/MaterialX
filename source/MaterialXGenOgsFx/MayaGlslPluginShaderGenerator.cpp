017406c5b1d478bae175553e3fa8cea8310ddc93
Niklas Harrysson <niklas.harrysson@gmail.com>
2019-07-04 16:51:42 +0200

    Fixed issue with static initialization order (#511)

--- source/MaterialXGenOgsFx/MayaGlslPluginShaderGenerator.cpp
+++ source/MaterialXGenOgsFx/MayaGlslPluginShaderGenerator.cpp
@@ -11,37 +11,18 @@
 namespace MaterialX
 {
 
-namespace
-{
-    static const string VIEW_POSITON_SEMANTIC = "ViewPosition";
-}
-
 const string MayaGlslPluginShaderGenerator::TARGET = "ogsfx_mayaglslplugin";
+
+MayaGlslPluginShaderGenerator::MayaGlslPluginShaderGenerator() :
+    OgsFxShaderGenerator()
+{
+    // Update view position semantic to match Maya's semantics.
+    _semanticsMap[HW::T_VIEW_POSITION] = "ViewPosition";
+}
 
 ShaderGeneratorPtr MayaGlslPluginShaderGenerator::create()
 {
     return std::make_shared<MayaGlslPluginShaderGenerator>();
-}
-
-ShaderPtr MayaGlslPluginShaderGenerator::createShader(const string& name, ElementPtr element, GenContext& context) const
-{
-    ShaderPtr shader = OgsFxShaderGenerator::createShader(name, element, context);
-
-    // Update view position semantic to match Maya's semantics.
-    for (size_t i = 0; i < shader->numStages(); ++i)
-    {
-        ShaderStage& stage = shader->getStage(i);
-        for (const auto& uniformMap : stage.getUniformBlocks())
-        {
-            ShaderPort* v = uniformMap.second->find(HW::T_VIEW_POSITION);
-            if (v)
-            {
-                v->setSemantic(VIEW_POSITON_SEMANTIC);
-            }
-        }
-    }
-
-    return shader;
 }
 
 void MayaGlslPluginShaderGenerator::getTechniqueParams(const Shader& shader, string& params) const
