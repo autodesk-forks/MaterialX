232fdea1521e576f3463d1ecacd626ccdf2d6a55
Pavlo Penenko <pavlo.penenko@autodesk.com>
2019-08-07 14:21:46 -0400

    Moved VP2 smart pointers to MayaUtil for better discoverability.

--- source/MaterialXContrib/MaterialXMaya/MayaUtil.h
+++ source/MaterialXContrib/MaterialXMaya/MayaUtil.h
@@ -1,14 +1,39 @@
 #ifndef MATERIALX_MAYA_MAYAUTIL_H
 #define MATERIALX_MAYA_MAYAUTIL_H
 
+#include <maya/MTextureManager.h>
+#include <maya/MStateManager.h>
+
 #include <string>
+#include <memory>
 
 namespace MaterialXMaya
 {
 namespace MayaUtil
 {
 void registerFragment(const std::string& fragmentName, const std::string& fragmentSource);
-}
-}
+
+struct TextureDeleter
+{
+    void operator()(MHWRender::MTexture* texture);
+};
+
+using TextureUniquePtr = std::unique_ptr<
+    MHWRender::MTexture,
+    TextureDeleter
+>;
+
+struct SamplerDeleter
+{
+    void operator()(const MHWRender::MSamplerState* sampler);
+};
+
+using SamplerUniquePtr = std::unique_ptr<
+    const MHWRender::MSamplerState,
+    SamplerDeleter
+>;
+
+} // namespace MayaUtil
+} // namespace MaterialXMaya
 
 #endif
