8e82f6df5f63df12155a99f69b5204d5b3f9a24b
Bernard Kwok <Bernard.Kwok@autodesk.com>
2019-09-17 14:20:38 -0400

    Review cleanup.

--- python/Scripts/mxdoc.py
+++ python/Scripts/mxdoc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-Verify that a specified file is a valid MaterialX document.
+Output documentation for nodedefs for a given MaterialX document.
 '''
 
 import sys, os, string; os.environ["PYTHONIOENCODING"] = "utf-8"
@@ -8,7 +8,7 @@ import MaterialX as mx
 
 def usage():
     print 'mxdoc.py: Output documentation for MaterialX nodedefs in Markdown format.'
-    print 'Usage:  mxvalidate.py <file.mtlx> [<outputfile.md]'
+    print 'Usage:  mxdoc.py <file.mtlx> [<outputfile.md>]'
     print '- Default output file name is "nodedef_documentation.md"'
 
 def main():
@@ -39,13 +39,12 @@ def main():
     file = open(outfilename, 'w+')
 
     nodedefs = doc.getNodeDefs()
-    typedefs = doc.getTypeDefs()
 
     for nd in nodedefs:
         file.write('- *Nodedef*: %s\n' % nd.getName())
         file.write('- *Type*: %s\n' % nd.getType())
         file.write('- *Doc*: %s\n' % nd.getAttribute('doc'))
-        file.write('| Name | Type | Default Value | UI min | UI max | UI group | Doc | Advanced | Connectable |\n')
+        file.write('| Name | Type | Default Value | UI min | UI max | UI group | Description | UI Advanced | Connectable |\n')
         file.write('| ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- |\n')
         for inp in nd.getInputs():
             name = inp.getName()
@@ -60,12 +59,14 @@ def main():
             file.write(buf)
         for p in nd.getParameters():
             name = p.getName()
+            type = p.getType();
+            value = inp.getValue()
             uimin = p.getAttribute('uimin')
             uimax = p.getAttribute('uimax')
             uifolder = p.getAttribute('uifolder')
             docattr = p.getAttribute('doc')
             adv = inp.getAttribute('uiadvanced')
-            buf = '| %s | %s | %s | %s | %s | %s | %s | %s | %s |\n' % (name, uimin, uimax, uifolder, docattr, adv, "false")
+            buf = '| %s | %s | %s | %s | %s | %s | %s | %s | %s |\n' % (name, type, value, uimin, uimax, uifolder, docattr, adv, "false")
             file.write(buf)
 
     file.close()
