c4c3c09f990e9609b63692950ff657951a0a6686
Ashwin Bhat <ashwin.bhat@autodesk.com>
2019-09-27 22:49:06 -0700

    Add vectorN variants of convert

--- source/PyMaterialX/PyMaterialXCore/PyUnitConverter.cpp
+++ source/PyMaterialX/PyMaterialXCore/PyUnitConverter.cpp
@@ -29,20 +29,66 @@ class PyUnitConverter : public mx::UnitConverter
             outputUnit
         );
     }
+
+    mx::Vector2 convert(mx::Vector2 input, const std::string& inputUnit, const std::string& outputUnit) const override
+    {
+        PYBIND11_OVERLOAD_PURE(
+            mx::Vector2,
+            mx::UnitConverter,
+            convert,
+            input,
+            inputUnit,
+            outputUnit
+        );
+    }
+
+    mx::Vector3 convert(mx::Vector3 input, const std::string& inputUnit, const std::string& outputUnit) const override
+    {
+        PYBIND11_OVERLOAD_PURE(
+            mx::Vector3,
+            mx::UnitConverter,
+            convert,
+            input,
+            inputUnit,
+            outputUnit
+        );
+    }
+
+    mx::Vector4 convert(mx::Vector4 input, const std::string& inputUnit, const std::string& outputUnit) const override
+    {
+        PYBIND11_OVERLOAD_PURE(
+            mx::Vector4,
+            mx::UnitConverter,
+            convert,
+            input,
+            inputUnit,
+            outputUnit
+        );
+    }
 };
 
 PYBIND11_DECLARE_HOLDER_TYPE(T, std::shared_ptr<T>);
 
 void bindPyUnitConverters(py::module& mod)
 {
+    /*
     py::class_<mx::UnitConverter, PyUnitConverter, mx::UnitConverterPtr>(mod, "UnitConverter")
-        .def("convert", &mx::UnitConverter::convert);
+        .def("convert", (float       (mx::UnitConverter::*)(float      , const std::string&, const std::string&)) &mx::UnitConverter::convert)
+        .def("convert", (mx::Vector2 (mx::UnitConverter::*)(mx::Vector4, const std::string&, const std::string&)) &mx::UnitConverter::convert)
+        .def("convert", (mx::Vector3 (mx::UnitConverter::*)(mx::Vector4, const std::string&, const std::string&)) &mx::UnitConverter::convert)
+        .def("convert", (mx::Vector4 (mx::UnitConverter::*)(mx::Vector4, const std::string&, const std::string&)) &mx::UnitConverter::convert);
+    */
 
     py::class_<mx::LengthUnitConverter, mx::UnitConverter, mx::LengthUnitConverterPtr>(mod, "LengthUnitConverter")
         .def_static("create", &mx::LengthUnitConverter::create)
         .def("getUnitScale", &mx::LengthUnitConverter::getUnitScale)
-        .def("getDefaultUnit", &mx::LengthUnitConverter::getDefaultUnit)
-        .def("convert", &mx::LengthUnitConverter::convert);
+        .def("getDefaultUnit", &mx::LengthUnitConverter::getDefaultUnit);
+    /*
+        .def("convert", (float       (mx::LengthUnitConverter::*)(float, const std::string&, const std::string&)) &mx::LengthUnitConverter::convert)
+        .def("convert", (mx::Vector2(mx::LengthUnitConverter::*)(mx::Vector4, const std::string&, const std::string&)) &mx::LengthUnitConverter::convert)
+        .def("convert", (mx::Vector3(mx::LengthUnitConverter::*)(mx::Vector4, const std::string&, const std::string&)) &mx::LengthUnitConverter::convert)
+        .def("convert", (mx::Vector4(mx::LengthUnitConverter::*)(mx::Vector4, const std::string&, const std::string&)) &mx::LengthUnitConverter::convert);
+    */
 
     py::class_<mx::UnitConverterRegistry, mx::UnitConverterRegistryPtr>(mod, "UnitConverterRegistry")
         .def("addUnitConverter", &mx::UnitConverterRegistry::addUnitConverter)
