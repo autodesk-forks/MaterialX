74655064c7b3ca25b0deeba5f5ce3f36c7fe8fb7
Pavlo Penenko <pavlo.penenko@autodesk.com>
2019-11-11 15:10:00 -0500

    Updated the test script to use a copy of mx_math.glsl.

--- source/MaterialXCross/cross_test.sh
+++ source/MaterialXCross/cross_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GLSLANG=../../../SPIRV-Cross/external/glslang-build/install/bin/glslangValidator
-$GLSLANG -H -i -S frag -Od --entry-point main --keep-uncalled -g ../../libraries/pbrlib/genglsl/lib/mx_math.glsl || exit 1
+$GLSLANG -H -i -S frag -Od --entry-point main --keep-uncalled -g ./test_mx_math.glsl || exit 1
 SPIRV_CROSS=../../../build/SPIRV-Cross/Debug/Debug/spirv-cross.exe
 $SPIRV_CROSS --hlsl --entry main --export-functions --output frag.hlsl frag.spv || exit 2
 
