1cf58bc3b512d77f7793fdd6187e684dc4d1894b
Pavlo Penenko <pavlo.penenko@autodesk.com>
2019-11-26 11:35:05 -0500

    Added license headers to MaterialXCross.

--- source/MaterialXCross/Cross.h
+++ source/MaterialXCross/Cross.h
@@ -1,3 +1,8 @@
+//
+// TM & (c) 2019 Lucasfilm Entertainment Company Ltd. and Lucasfilm Ltd.
+// All rights reserved.  See LICENSE.txt for license.
+//
+
 #ifndef MATERIALX_CROSS_H
 #define MATERIALX_CROSS_H
 
