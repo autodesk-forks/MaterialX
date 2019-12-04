ac042caf126af6c3468003f22e3c6717f620dd8f
Bernard Kwok <Bernard.Kwok@autodesk.com>
2019-12-04 14:12:12 -0500

    Fix function names.

--- libraries/stdlib/genglsl/mx_transformmatrix_vector2M3.glsl
+++ libraries/stdlib/genglsl/mx_transformmatrix_vector2M3.glsl
@@ -1,4 +1,4 @@
-void transformpoint2_matrix33(vec2 val, mat3 transform, out vec2 result)
+void mx_transformmatrix_vector2M3(vec2 val, mat3 transform, out vec2 result)
 {
   vec3 res = transform * vec3(val, 1.0);
   result = res.xy;
--- libraries/stdlib/genglsl/mx_transformmatrix_vector3M4.glsl
+++ libraries/stdlib/genglsl/mx_transformmatrix_vector3M4.glsl
@@ -1,4 +1,4 @@
-void transformpoint3_matrix44(vec3 val, mat4 transform, out vec3 result)
+void mx_transformmatrix_vector3M4(vec3 val, mat4 transform, out vec3 result)
 {
   vec4 res = transform * vec4(val, 1.0);
   result = res.xyz;
