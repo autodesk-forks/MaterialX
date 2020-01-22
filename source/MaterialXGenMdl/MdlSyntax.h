f868a0fbe94ce2f32e5fde0d6a86a9ae45df3c51
Niklas Harrysson <niklas.harrysson@autodesk.com>
2020-01-22 13:09:23 +0100

    Update MDL syntax class

--- source/MaterialXGenMdl/MdlSyntax.h
+++ source/MaterialXGenMdl/MdlSyntax.h
@@ -27,10 +27,11 @@ public:
     const string& getConstantQualifier() const override { return CONST_QUALIFIER; };
 
     static const string CONST_QUALIFIER;
-    static const StringVec VECTOR_MEMBERS;
     static const StringVec VECTOR2_MEMBERS;
+    static const StringVec VECTOR3_MEMBERS;
     static const StringVec VECTOR4_MEMBERS;
     static const StringVec COLOR2_MEMBERS;
+    static const StringVec COLOR3_MEMBERS;
     static const StringVec COLOR4_MEMBERS;
 };
 
