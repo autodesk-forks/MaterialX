4edfedbaa84a3ef8ef23e7252f6afa9facd2e752
Jonathan Feldstein <jonathan.feldstein@autodesk.com>
2020-01-24 15:59:54 -0500

    Adds support for a StageManager (test included).

---
+++ source/MaterialXRuntime/Private/PvtStageManager.h
@@ -0,0 +1,43 @@
+//
+// TM & (c) 2020 Lucasfilm Entertainment Company Ltd. and Lucasfilm Ltd.
+// All rights reserved.  See LICENSE.txt for license.
+//
+
+#ifndef MATERIALX_PVTSTAGEMANAGER_H
+#define MATERIALX_PVTSTAGEMANAGER_H
+
+#include <MaterialXRuntime/Library.h>
+#include <MaterialXRuntime/RtObject.h>
+
+#include <MaterialXFormat/File.h>
+
+#include <unordered_map>
+
+namespace MaterialX
+{
+
+class PvtStageManager
+{
+  public:
+    /// Constructor
+    PvtStageManager();
+
+    /// Creates a stage that is referenced by the scene stage
+    RtObject createStage(const string& stageName);
+
+    /// Deletes a stage known to the stage manager
+    void deleteStage(const string& stageName);
+
+    /// Whether the stage manager contains a stage with the given name
+    bool hasStage(const string& stageName) const;
+
+    /// Retrieves a stage from the stage manager
+    RtObject getStageObject(const string& stageName) const;
+
+  private:
+    std::unordered_map<string, RtObject> _stages;
+};
+
+}
+
+#endif
