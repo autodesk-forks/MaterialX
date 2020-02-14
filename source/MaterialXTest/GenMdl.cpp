ba21adf2f1d43222eac5a239be81d31d14c9270e
Bernard Kwok <Bernard.Kwok@autodesk.com>
2020-02-14 16:10:11 -0500

    Call mdlc if available and log error output.

--- source/MaterialXTest/GenMdl.cpp
+++ source/MaterialXTest/GenMdl.cpp
@@ -143,6 +143,16 @@ void MdlShaderGeneratorTester::compileSource(const std::vector<mx::FilePath>& so
     mdlcCommand += " > " + errorFile.asString() + " 2>&1";
 
     _logFile << mdlcCommand << std::endl;
+
+    int returnValue = std::system(mdlcCommand.c_str());
+    _logFile << "\tReturn code: " << std::to_string(returnValue) << std::endl;
+    std::ifstream errorStream(errorFile);
+    mx::StringVec result;
+    std::string line;
+    while (std::getline(errorStream, line))
+    {
+        _logFile << "\tError: " << line << std::endl;
+    }
 }
 
 
