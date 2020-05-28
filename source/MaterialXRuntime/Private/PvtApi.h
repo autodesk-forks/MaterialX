192febd98103ef91ab4efd339fbe0ddd8315b83d
Bernard Kwok <Bernard.Kwok@autodesk.com>
2020-05-28 13:23:54 -0400

    Add in user definition location global.

--- source/MaterialXRuntime/Private/PvtApi.h
+++ source/MaterialXRuntime/Private/PvtApi.h
@@ -192,6 +192,16 @@ public:
         return names;
     }
 
+   const FilePath& getUserDefinitionPath()
+    {
+        return _userDefinitionLocation;
+    }
+
+    void setUserDefinitionPath(const FilePath& path)
+    {
+        _userDefinitionLocation = path;
+    }
+
     RtToken makeUniqueName(const RtToken& name) const;
 
     RtStagePtr createStage(const RtToken& name)
@@ -255,6 +265,7 @@ public:
     FileSearchPath _searchPaths;
     FileSearchPath _implementationSearchPaths;
     FileSearchPath _textureSearchPaths;
+    FilePath _userDefinitionLocation;
     RtStagePtr _libraryRoot;
     RtTokenMap<RtStagePtr> _libraries;
     UnitConverterRegistryPtr  _unitDefinitions;
