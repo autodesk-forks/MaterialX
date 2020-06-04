c40ae2fe29dbf3448b5cee2d1dbf0d164c3abaaa
Niklas Harrysson <niklas.harrysson@gmail.com>
2020-06-04 11:59:49 +0200

    Using directional albedo for layering in OSL.

--- source/MaterialXGenGlsl/GlslShaderGenerator.cpp
+++ source/MaterialXGenGlsl/GlslShaderGenerator.cpp
@@ -422,7 +422,7 @@ void GlslShaderGenerator::emitPixelStage(const ShaderGraph& graph, GenContext& c
         const unsigned int maxLights = std::max(1u, context.getOptions().hwMaxActiveLightSources);
         emitLine("#define MAX_LIGHT_SOURCES " + std::to_string(maxLights), stage, false);
     }
-    emitLine("#define DIRECTIONAL_ALBEDO_METHOD " + std::to_string(int(context.getOptions().hwDirectionalAlbedoMethod)), stage, false);
+    emitLine("#define DIRECTIONAL_ALBEDO_METHOD " + std::to_string(int(context.getOptions().directionalAlbedoMethod)), stage, false);
     emitLineBreak(stage);
     emitTypeDefinitions(context, stage);
 
@@ -515,8 +515,8 @@ void GlslShaderGenerator::emitPixelStage(const ShaderGraph& graph, GenContext& c
     }
 
     // Emit directional albedo table code.
-    if (context.getOptions().hwDirectionalAlbedoMethod == DIRECTIONAL_ALBEDO_TABLE ||
-        context.getOptions().hwWriteAlbedoTable)
+    if (context.getOptions().directionalAlbedoMethod == DIRECTIONAL_ALBEDO_TABLE ||
+        context.getOptions().writeDirectionalAlbedoTable)
     {
         emitInclude("pbrlib/" + GlslShaderGenerator::LANGUAGE + "/lib/mx_table.glsl", context, stage);
         emitLineBreak(stage);
@@ -560,7 +560,7 @@ void GlslShaderGenerator::emitPixelStage(const ShaderGraph& graph, GenContext& c
     {
         emitLine(outputSocket->getVariable() + " = vec4(mx_compute_depth_moments(), 0.0, 1.0)", stage);
     }
-    else if (context.getOptions().hwWriteAlbedoTable)
+    else if (context.getOptions().writeDirectionalAlbedoTable)
     {
         emitLine(outputSocket->getVariable() + " = vec4(mx_ggx_directional_albedo_generate_table(), 0.0, 1.0)", stage);
     }
