170ae82d1ee4f99963a48ab9c741bbb2393d39b6
Jonathan Feldstein <jonathan.feldstein@autodesk.com>
2020-07-09 11:33:42 -0400

    Adds support for setting the uniform flag on inputs and enables publishing of uniforms.

--- source/MaterialXRuntime/RtStage.cpp
+++ source/MaterialXRuntime/RtStage.cpp
@@ -174,7 +174,7 @@ RtPrim RtStage::createNodeDef(RtNodeGraph& nodeGraph,
     // Add an input per nodegraph input
     for (auto input : nodeGraph.getInputs())
     {
-        RtInput attr = nodedef.createInput(input.getName(), input.getType());
+        RtInput attr = nodedef.createInput(input.getName(), input.getType(), input.asA<RtInput>().isUniform() ? RtAttrFlag::UNIFORM : 0);
 
         const PvtObject* obj = PvtObject::hnd(input)->asA<PvtObject>();
         const vector<RtToken>& metadataNames = obj->getMetadataOrder();
