df29f647dc4f0bd519fab5605e1990a6ddc086c0
Niklas Harrysson <niklas.harrysson@gmail.com>
2020-07-15 09:47:08 +0200

    Additional fix for unittest failure.

--- source/MaterialXTest/MaterialXGenShader/GenShaderUtil.cpp
+++ source/MaterialXTest/MaterialXGenShader/GenShaderUtil.cpp
@@ -662,11 +662,6 @@ void ShaderGeneratorTester::validate(const mx::GenOptions& generateOptions, cons
         // For each new file clear the implementation cache.
         // Since the new file might contain implementations with names
         // colliding with implementations in previous test cases.
-        // Save the used implementation names first if we are set to track this.
-        if (options.checkImplCount)
-        {
-            context.getNodeImplementationNames(_usedImplementations);
-        }
         context.clearNodeImplementations();
 
         // Set user data
@@ -797,17 +792,20 @@ void ShaderGeneratorTester::validate(const mx::GenOptions& generateOptions, cons
                 mx::InterfaceElementPtr impl = nodeDef->getImplementation(_shaderGenerator->getTarget(), _shaderGenerator->getLanguage());
                 if (impl)
                 {
+                    _logFile << "------------ Run validation with element: " << namePath << "------------" << std::endl;
+
+                    mx::StringVec sourceCode;
+                    bool generatedCode = generateCode(context, elementName, targetElement, _logFile, _testStages, sourceCode);
+
                     // Record implementations tested
                     if (options.checkImplCount)
                     {
+                        context.getNodeImplementationNames(_usedImplementations);
                         mx::NodeGraphPtr nodeGraph = impl->asA<mx::NodeGraph>();
                         mx::InterfaceElementPtr nodeGraphImpl = nodeGraph ? nodeGraph->getImplementation() : nullptr;
                         _usedImplementations.insert(nodeGraphImpl ? nodeGraphImpl->getName() : impl->getName());
                     }
 
-                    _logFile << "------------ Run validation with element: " << namePath << "------------" << std::endl;
-                    mx::StringVec sourceCode;
-                    bool generatedCode = generateCode(context, elementName, targetElement, _logFile, _testStages, sourceCode);
                     if (!generatedCode)
                     {
                         _logFile << ">> Failed to generate code for nodedef: " << nodeDefName << std::endl;
