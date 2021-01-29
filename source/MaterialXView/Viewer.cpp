6cd2f1356ff7e169b531ce3c9b759aea0fdc637e
Jonathan Stone <jstone@lucasfilm.com>
2021-01-28 21:03:27 -0800

    Improvements to specular environment rendering
    - Add a missing geometric term to the prefiltered environment path.
    - Improve sharing and alignment between environment rendering paths.
    - Display shader generation errors in the console when compiling viewer materials.

--- source/MaterialXView/Viewer.cpp
+++ source/MaterialXView/Viewer.cpp
@@ -1266,6 +1266,14 @@ void Viewer::loadDocument(const mx::FilePath& filename, mx::DocumentPtr librarie
             }
         }
     }
+    catch (mx::ExceptionShaderRenderError& e)
+    {
+        for (const std::string& error : e.errorLog())
+        {
+            std::cerr << error << std::endl;
+        }
+        new ng::MessageDialog(this, ng::MessageDialog::Type::Warning, "Shader generation error", e.what());
+    }
     catch (std::exception& e)
     {
         new ng::MessageDialog(this, ng::MessageDialog::Type::Warning, "Failed to load material", e.what());
@@ -1287,11 +1295,22 @@ void Viewer::reloadShaders()
         {
             material->generateShader(_genContext);
         }
+        return;
+    }
+    catch (mx::ExceptionShaderRenderError& e)
+    {
+        for (const std::string& error : e.errorLog())
+        {
+            std::cerr << error << std::endl;
+        }
+        new ng::MessageDialog(this, ng::MessageDialog::Type::Warning, "Shader generation error", e.what());
     }
     catch (std::exception& e)
     {
-        new ng::MessageDialog(this, ng::MessageDialog::Type::Warning, "Shader Generation Error", e.what());
+        new ng::MessageDialog(this, ng::MessageDialog::Type::Warning, "Failed to reload shaders", e.what());
     }
+
+    _materials.clear();
 }
 
 void Viewer::saveShaderSource(mx::GenContext& context)
