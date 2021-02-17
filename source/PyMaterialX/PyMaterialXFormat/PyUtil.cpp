667ee28e58776b6fd0607e3f96cf8eb76fb0dede
Niklas Harrysson <niklas.harrysson@gmail.com>
2021-02-17 09:33:03 +0100

    Revert python binding change

--- source/PyMaterialX/PyMaterialXFormat/PyUtil.cpp
+++ source/PyMaterialX/PyMaterialXFormat/PyUtil.cpp
@@ -16,13 +16,11 @@ void bindPyUtil(py::module& mod)
 {
     mod.def("readFile", &mx::readFile);
     mod.def("getSubdirectories", &mx::getSubdirectories);
-    mod.def("loadDocuments", &mx::loadDocuments,
-        py::arg("rootPath"), py::arg("searchPath"), py::arg("skipFiles"), py::arg("includeFiles"), py::arg("documents"), py::arg("documentsPaths"),
-        py::arg("readOptions") = (mx::XmlReadOptions*) nullptr, py::arg("errors") = (mx::StringVec*) nullptr);
+    mod.def("loadDocuments", &mx::loadDocuments);
     mod.def("loadLibrary", &mx::loadLibrary,
         py::arg("file"), py::arg("doc"), py::arg("searchPath") = mx::FileSearchPath(), py::arg("readOptions") = (mx::XmlReadOptions*) nullptr);
     mod.def("loadLibraries", &mx::loadLibraries,
-        py::arg("libraryFolders"), py::arg("searchPath"), py::arg("doc"), py::arg("excludeFiles") = (mx::StringSet*) nullptr, py::arg("readOptions") = (mx::XmlReadOptions*) nullptr);
+        py::arg("libraryFolders"), py::arg("searchPath"), py::arg("doc"), py::arg("excludeFiles") = mx::StringSet(), py::arg("readOptions") = (mx::XmlReadOptions*) nullptr);
     mod.def("flattenFilenames", &mx::flattenFilenames,
         py::arg("doc"), py::arg("searchPath") = mx::FileSearchPath(), py::arg("customResolver") = (mx::StringResolverPtr) nullptr);
 }
