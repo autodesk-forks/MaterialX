2c1b9122b12c90699628637db17749edee32ee73
kohakukun <aura.munoz@autodesk.com>
2021-05-26 13:11:17 +0200

    rename util binding

--- source/JsMaterialX/JsMaterialXFormat/JsUtil.cpp
+++ source/JsMaterialX/JsMaterialXFormat/JsUtil.cpp
@@ -6,7 +6,7 @@
 namespace ems = emscripten;
 namespace mx = MaterialX;
 
-EMSCRIPTEN_BINDINGS(util)
+EMSCRIPTEN_BINDINGS(xformatUtil)
 {
     ems::constant("PATH_LIST_SEPARATOR", mx::PATH_LIST_SEPARATOR);
     ems::constant("MATERIALX_SEARCH_PATH_ENV_VAR", mx::MATERIALX_SEARCH_PATH_ENV_VAR);
