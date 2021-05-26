6dcc3fdb347844e9c60887aefbf11aa94a5c894b
kohakukun <aura.munoz@autodesk.com>
2021-05-26 14:05:48 +0200

    add loadLibrary

--- source/JsMaterialX/JsMaterialXFormat/JsUtil.cpp
+++ source/JsMaterialX/JsMaterialXFormat/JsUtil.cpp
@@ -1,3 +1,5 @@
+#include "../Helpers.h"
+#include "./emscriptenTypeRegistration.h"
 #include <MaterialXFormat/Util.h>
 
 #include <emscripten.h>
@@ -8,9 +10,10 @@ namespace mx = MaterialX;
 
 EMSCRIPTEN_BINDINGS(xformatUtil)
 {
-    ems::constant("PATH_LIST_SEPARATOR", mx::PATH_LIST_SEPARATOR);
-    ems::constant("MATERIALX_SEARCH_PATH_ENV_VAR", mx::MATERIALX_SEARCH_PATH_ENV_VAR);
-    ems::constant("MATERIALX_ASSET_DEFINITION_PATH_ENV_VAR", mx::MATERIALX_ASSET_DEFINITION_PATH_ENV_VAR);
-    ems::constant("MATERIALX_ASSET_TEXTURE_PATH_ENV_VAR", mx::MATERIALX_ASSET_TEXTURE_PATH_ENV_VAR);
+  ems::constant("PATH_LIST_SEPARATOR", mx::PATH_LIST_SEPARATOR);
+  ems::constant("MATERIALX_SEARCH_PATH_ENV_VAR", mx::MATERIALX_SEARCH_PATH_ENV_VAR);
+  ems::constant("MATERIALX_ASSET_DEFINITION_PATH_ENV_VAR", mx::MATERIALX_ASSET_DEFINITION_PATH_ENV_VAR);
+  ems::constant("MATERIALX_ASSET_TEXTURE_PATH_ENV_VAR", mx::MATERIALX_ASSET_TEXTURE_PATH_ENV_VAR);
+  BIND_FUNC_RAW_PTR("loadLibrary", mx::loadLibrary, 2, 4, const mx::FilePath&, mx::DocumentPtr, const mx::FileSearchPath&, const mx::XmlReadOptions*);
 }
 
--- source/JsMaterialX/test/XmlIo.spec.js
+++ source/JsMaterialX/test/XmlIo.spec.js
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { initMaterialX } from './testHelpers';
 import { expect } from 'chai';
+import * as fs from 'fs';
 
 const resourcesBasePath = path.resolve('../../../resources/Materials/Examples/Syntax');
 
@@ -9,6 +10,16 @@ describe('XmlIo', () => {
   before(async () => {
       mx = await initMaterialX();
   });
+
+  const loadFileSysmtem = (filePath) => {
+    const file = fs.readFileSync(filePath);
+    const dir = path.dirname(filePath);
+    const dirStats = mx.FS.analyzePath(dir);
+    if (!dirStats.exists) {
+      mx.FS.createPath('/', dir, true, true);
+    }
+    mx.FS.createDataFile(dir, path.basename(filePath), new Uint8Array(file), true, true, true);
+  }
 
   it('should read simple file', async () => {
     const doc = mx.createDocument();
@@ -29,6 +40,15 @@ describe('XmlIo', () => {
     mx.prependXInclude(doc, includePath);
     const xmlString = mx.writeToXmlString(doc, writeOptions);
     expect(xmlString).to.include(xmlString);
+  });
+
+  it('load library', () => {
+    const doc = mx.createDocument();
+    const filepath = path.resolve(resourcesBasePath, 'SimpleSrf.mtlx');
+    loadFileSysmtem(filepath);
+    mx.loadLibrary(filepath, doc);
+    const xmlString = mx.writeToXmlString(doc, new mx.XmlWriteOptions());
+    expect(xmlString).to.include(filepath);
   });
 });
 
