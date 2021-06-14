33dfcb8025b75d7e0cdbb546aa9d2cb90251e2fe
Jonathan Feldstein <jonathan.feldstein@autodesk.com>
2021-06-14 15:32:25 -0400

    Added MX_CORE_API to LookUtil functions.

--- source/MaterialXCore/LookUtil.h
+++ source/MaterialXCore/LookUtil.h
@@ -6,24 +6,26 @@
 #ifndef MATERIALX_LOOK_UTIL_H
 #define MATERIALX_LOOK_UTIL_H
 
+#include <MaterialXCore/Export.h>
+
 #include <MaterialXCore/Look.h>
 
 namespace MaterialX
 {
 
 // Return the list of all look elements which are active
-LookVec getActiveLooks(const LookGroupPtr& lookGroup);
+MX_CORE_API LookVec getActiveLooks(const LookGroupPtr& lookGroup);
 
 /// Append the contents of another lookgroup to this lookgroup.
 /// Optionally allow appending after a given look.
-void appendLookGroup(LookGroupPtr& lookGroup, const LookGroupPtr& lookGroupToAppend, const string& appendAfterLook = EMPTY_STRING);
+MX_CORE_API void appendLookGroup(LookGroupPtr& lookGroup, const LookGroupPtr& lookGroupToAppend, const string& appendAfterLook = EMPTY_STRING);
 
 /// Append the contents of a look to this lookgroup
 /// Optionally allow appending after a given look.
-void appendLook(LookGroupPtr& lookGroup, const string& lookName, const string& appendAfterLook = EMPTY_STRING);
+MX_CORE_API void appendLook(LookGroupPtr& lookGroup, const string& lookName, const string& appendAfterLook = EMPTY_STRING);
 
 /// Get a single combined look wihch contains the contents of all the looks in the lookgroup
-LookPtr combineLooks(const LookGroupPtr& lookGroup);
+MX_CORE_API LookPtr combineLooks(const LookGroupPtr& lookGroup);
 
 } // namespace MaterialX
 
