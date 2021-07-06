941cb1409a24e2f073e31176ed6abf03b56799fb
Philipp Frericks <philipp.frericks@autodesk.com>
2021-07-06 12:22:41 +0200

    Wrap all functions that return value pointers

--- source/JsMaterialX/JsMaterialXCore/post.js
+++ source/JsMaterialX/JsMaterialXCore/post.js
@@ -6,12 +6,41 @@
 // Wrapping code in an anonymous function to prevent clashes with the main module and other pre / post JS.
 (function () {
     onModuleReady(function() {
-    Module.ValueElement.prototype.getValue = function () {
-        const value = Module.ValueElement.prototype._getValue.apply(this);
-        if (value) {
-        return value.getData();
+        var valueGetter = {
+            AttributeDef: [
+                "getValue"
+            ],
+            Document: [
+                "getGeomPropValue"
+            ],
+            ValueElement: [
+                "getValue",
+                "getResolvedValue",
+                "getDefaultValue"
+            ],
+            InterfaceElement: [
+                "getInputValue"
+            ],
+            PropertySet: [
+                "getPropertyValue"
+            ]
+        };
+
+        function makeWrapper(c, f) {
+            return function () {
+                var funcName = "_" + f;
+                var value = Module[c].prototype[funcName].apply(this, arguments);
+                if (value) {
+                    return value.getData();
+                }
+                return value;
+            }
         }
-        return value;
-    }
+
+        for (var c of Object.keys(valueGetter)) {
+            for (var f of valueGetter[c]) {
+                Module[c].prototype[f] = makeWrapper(c, f);
+            }
+        }
     });
 })();
