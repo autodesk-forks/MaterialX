f53a0a5928c907537db9450a057fb8bcbddcb696
Philipp Frericks <philipp.frericks@autodesk.com>
2021-07-23 12:35:49 +0200

    Return null if not value is set

--- source/JsMaterialX/JsMaterialXCore/post.js
+++ source/JsMaterialX/JsMaterialXCore/post.js
@@ -57,7 +57,7 @@
                 if (value) {
                     return value.getData();
                 }
-                return value;
+                return null;
             }
         }
 
