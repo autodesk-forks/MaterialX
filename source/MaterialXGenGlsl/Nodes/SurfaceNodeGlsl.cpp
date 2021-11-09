4dca95b88624f33b31c5622c918acfc5c6c456b0
Niklas Harrysson <niklas.harrysson@autodesk.com>
2021-11-09 14:45:37 +0100

    Fix regression with vertex data prefix.

--- source/MaterialXGenGlsl/Nodes/SurfaceNodeGlsl.cpp
+++ source/MaterialXGenGlsl/Nodes/SurfaceNodeGlsl.cpp
@@ -108,9 +108,9 @@ void SurfaceNodeGlsl::emitFunctionCall(const ShaderNode& node, GenContext& conte
 
         shadergen.emitScopeBegin(stage);
 
-        shadergen.emitLine("vec3 N = normalize(" + HW::T_VERTEX_DATA_INSTANCE + "." + HW::T_NORMAL_WORLD + ")", stage);
-        shadergen.emitLine("vec3 V = normalize(" + HW::T_VIEW_POSITION + " - " + HW::T_VERTEX_DATA_INSTANCE + "." + HW::T_POSITION_WORLD + ")", stage);
-        shadergen.emitLine("vec3 P = " + HW::T_VERTEX_DATA_INSTANCE + "." + HW::T_POSITION_WORLD, stage);
+        shadergen.emitLine("vec3 N = normalize(" + prefix + HW::T_NORMAL_WORLD + ")", stage);
+        shadergen.emitLine("vec3 V = normalize(" + HW::T_VIEW_POSITION + " - " + prefix + HW::T_POSITION_WORLD + ")", stage);
+        shadergen.emitLine("vec3 P = " + prefix + HW::T_POSITION_WORLD, stage);
         shadergen.emitLineBreak(stage);
 
         const string outColor = output->getVariable() + ".color";
@@ -135,7 +135,7 @@ void SurfaceNodeGlsl::emitFunctionCall(const ShaderNode& node, GenContext& conte
             shadergen.emitComment("Shadow occlusion", stage);
             if (context.getOptions().hwShadowMap)
             {
-                shadergen.emitLine("vec3 shadowCoord = (" + HW::T_SHADOW_MATRIX + " * vec4(" + HW::T_VERTEX_DATA_INSTANCE + "." + HW::T_POSITION_WORLD + ", 1.0)).xyz", stage);
+                shadergen.emitLine("vec3 shadowCoord = (" + HW::T_SHADOW_MATRIX + " * vec4(" + prefix + HW::T_POSITION_WORLD + ", 1.0)).xyz", stage);
                 shadergen.emitLine("shadowCoord = shadowCoord * 0.5 + 0.5", stage);
                 shadergen.emitLine("vec2 shadowMoments = texture(" + HW::T_SHADOW_MAP + ", shadowCoord.xy).xy", stage);
                 shadergen.emitLine("float occlusion = mx_variance_shadow_occlusion(shadowMoments, shadowCoord.z)", stage);
@@ -235,6 +235,8 @@ void SurfaceNodeGlsl::emitLightLoop(const ShaderNode& node, GenContext& context,
     if (context.getOptions().hwMaxActiveLightSources > 0)
     {
         const GlslShaderGenerator& shadergen = static_cast<const GlslShaderGenerator&>(context.getShaderGenerator());
+        const VariableBlock& vertexData = stage.getInputBlock(HW::VERTEX_DATA);
+        const string prefix = shadergen.getVertexDataPrefix(vertexData);
 
         const ShaderInput* bsdfInput = node.getInput("bsdf");
         const ShaderNode* bsdf = bsdfInput->getConnectedSibling();
@@ -246,7 +248,7 @@ void SurfaceNodeGlsl::emitLightLoop(const ShaderNode& node, GenContext& context,
 
         shadergen.emitScopeBegin(stage);
 
-        shadergen.emitLine("sampleLightSource(" + HW::T_LIGHT_DATA_INSTANCE + "[activeLightIndex], " + HW::T_VERTEX_DATA_INSTANCE + "." + HW::T_POSITION_WORLD + ", lightShader)", stage);
+        shadergen.emitLine("sampleLightSource(" + HW::T_LIGHT_DATA_INSTANCE + "[activeLightIndex], " + prefix + HW::T_POSITION_WORLD + ", lightShader)", stage);
         shadergen.emitLine("vec3 L = lightShader.direction", stage);
         shadergen.emitLineBreak(stage);
 
