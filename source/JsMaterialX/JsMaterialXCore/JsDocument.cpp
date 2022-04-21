23656f4181437ee735a25c04b33cad73ca110739
Ashwin Bhat <ashwin.bhat@autodesk.com>
2022-04-20 18:06:28 -0700

    Build fix

--- source/JsMaterialX/JsMaterialXCore/JsDocument.cpp
+++ source/JsMaterialX/JsMaterialXCore/JsDocument.cpp
@@ -61,7 +61,7 @@ EMSCRIPTEN_BINDINGS(document)
         .function("removeTypeDef", &mx::Document::removeTypeDef)
         BIND_MEMBER_FUNC("addNodeDef", mx::Document, addNodeDef, 0, 3, stRef, stRef, stRef)
         BIND_MEMBER_FUNC("addNodeDefFromGraph", mx::Document, addNodeDefFromGraph, 7, 8, const mx::NodeGraphPtr, 
-            stRef, stRef, stRef, bool, stRef, std::string, stRef)
+            stRef, stRef, stRef, bool, stRef, std::string)
         .function("getNodeDef", &mx::Document::getNodeDef)
         .function("getNodeDefs", &mx::Document::getNodeDefs)
         .function("removeNodeDef", &mx::Document::removeNodeDef)
